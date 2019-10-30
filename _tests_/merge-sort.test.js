878cdbccaeee6140cb65672794d1822472206775
allisonbusse <allisonbusse@gmail.com>
2019-10-29 20:19:19 -0700

    additional test

--- _tests_/merge-sort.test.js
+++ _tests_/merge-sort.test.js
@@ -6,4 +6,10 @@ describe('Merge Sort Algorithm', () => {
     mergeSort(array);
     expect(array).toEqual([1, 2, 4, 6, 12, 100]);
   });
+
+  it('sorts an array with an odd number', () => {
+    const array = [6, 2, 4, 12, 18, 100, 1];
+    mergeSort(array);
+    expect(array).toEqual([1, 2, 4, 6, 12, 18, 100]);
+  });
 });
